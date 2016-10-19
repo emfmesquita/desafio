e1929bbc98fceac5c19089baf835f06a0aeb6d0e
emfmesquita <emesquita@controltechnologysolutions.com>
2016-10-19 05:49:41 -0200

    Epicon Service

---
+++ src/main/java/com/edson/core/EpiconService.java
@@ -0,0 +1,77 @@
+package com.edson.core;
+
+import java.io.IOException;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.stereotype.Component;
+
+import com.edson.entity.SKU;
+import com.google.gson.Gson;
+
+@Component
+public class EpiconService {
+
+	private static String BASE_REQUEST_URL = "https://sandboxmhubapi.epicom.com.br/v1/marketplace/produtos/%s/skus/%s";
+	private static String USERNAME = "897F8D21A9F5A";
+	private static String PASSWORD = "Ip15q6u7X15EP22GS36XoNLrX2Jz0vqq";
+
+	private static String RESPONSE_FAIL_MESSAGE = "Response fail with status %d:\n%s";
+	private static String RESPONSE_PARSE_FAIL_MESSAGE = "Fail to parse sku from:\n%s";
+
+	public SKU getSku(String productId, String skuId) {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		try {
+			HttpGet httpget = buildRequest(productId, skuId);
+			;
+			CloseableHttpResponse response = httpclient.execute(httpget);
+			try {
+				int statusCode = response.getStatusLine().getStatusCode();
+				String responseData = EntityUtils.toString(response.getEntity());
+				if (200 != statusCode) {
+					throw new RuntimeException(String.format(RESPONSE_FAIL_MESSAGE, statusCode, responseData));
+				}
+				return toSku(responseData);
+			} finally {
+				response.close();
+			}
+		} catch (ClientProtocolException e) {
+			throw new RuntimeException("Fail to get sku info from epicon", e);
+		} catch (IOException e) {
+			throw new RuntimeException("Fail to get sku info from epicon", e);
+		} finally {
+			try {
+				httpclient.close();
+			} catch (IOException e) {
+				// only tries to close
+			}
+		}
+	}
+
+	private HttpGet buildRequest(String productId, String skuId) {
+		String url = String.format(BASE_REQUEST_URL, productId, skuId);
+		HttpGet httpget = new HttpGet(url);
+		httpget.addHeader("Authorization", buildAuthHeader());
+		return httpget;
+	}
+
+	private String buildAuthHeader() {
+		return "Basic " + Base64.encodeBase64String((USERNAME + ":" + PASSWORD).getBytes());
+	}
+
+	private SKU toSku(String responseData) {
+		Gson gson = new Gson();
+		SKU sku = null;
+		try {
+			sku = gson.fromJson(responseData, SKU.class);
+		} catch (Exception e) {
+			throw new RuntimeException(String.format(RESPONSE_PARSE_FAIL_MESSAGE, responseData));
+		}
+		return sku;
+	}
+}
--- src/main/java/com/edson/entity/SKU.java
+++ src/main/java/com/edson/entity/SKU.java
@@ -16,6 +16,15 @@ public class SKU implements Serializable {
 	private static final long serialVersionUID = -6548410729330199029L;
 
 	private String id;
+	private String nome;
+	private String codigo;
+	private String modelo;
+	private Boolean foraDeLinha;
+	private String preco;
+	private String precoDe;
+	private Boolean disponivel;
+	private Integer estoque;
+	private Boolean ativo;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "desafio")
@@ -28,11 +37,91 @@ public class SKU implements Serializable {
 		this.id = id;
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public Boolean getForaDeLinha() {
+		return foraDeLinha;
+	}
+
+	public void setForaDeLinha(Boolean foraDeLinha) {
+		this.foraDeLinha = foraDeLinha;
+	}
+
+	public String getPreco() {
+		return preco;
+	}
+
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+
+	public String getPrecoDe() {
+		return precoDe;
+	}
+
+	public void setPrecoDe(String precoDe) {
+		this.precoDe = precoDe;
+	}
+
+	public Boolean getDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(Boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((ativo == null) ? 0 : ativo.hashCode());
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		result = prime * result + ((disponivel == null) ? 0 : disponivel.hashCode());
+		result = prime * result + ((estoque == null) ? 0 : estoque.hashCode());
+		result = prime * result + ((foraDeLinha == null) ? 0 : foraDeLinha.hashCode());
+		result = prime * result + ((modelo == null) ? 0 : modelo.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+		result = prime * result + ((precoDe == null) ? 0 : precoDe.hashCode());
 		return result;
 	}
 
@@ -45,10 +134,50 @@ public class SKU implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		SKU other = (SKU) obj;
-		if (id == null) {
-			if (other.id != null)
+		if (ativo == null) {
+			if (other.ativo != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!ativo.equals(other.ativo))
+			return false;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		if (disponivel == null) {
+			if (other.disponivel != null)
+				return false;
+		} else if (!disponivel.equals(other.disponivel))
+			return false;
+		if (estoque == null) {
+			if (other.estoque != null)
+				return false;
+		} else if (!estoque.equals(other.estoque))
+			return false;
+		if (foraDeLinha == null) {
+			if (other.foraDeLinha != null)
+				return false;
+		} else if (!foraDeLinha.equals(other.foraDeLinha))
+			return false;
+		if (modelo == null) {
+			if (other.modelo != null)
+				return false;
+		} else if (!modelo.equals(other.modelo))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (preco == null) {
+			if (other.preco != null)
+				return false;
+		} else if (!preco.equals(other.preco))
+			return false;
+		if (precoDe == null) {
+			if (other.precoDe != null)
+				return false;
+		} else if (!precoDe.equals(other.precoDe))
 			return false;
 		return true;
 	}
